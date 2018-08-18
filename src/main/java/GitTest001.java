2a6cf42551f90326696780090c1815506216fbc9
caoyinbao <879992127@qq.com>
2018-08-18 21:23:16 +0800

    删减002分支代码

--- src/main/java/GitTest001.java
+++ src/main/java/GitTest001.java
@@ -1,8 +1,5 @@
 public class GitTest001 {
     public static void main(String[] args) {
         System.out.println("12345");
-        System.out.println("abcde");
-        System.out.println("`~!@#$");
-        System.out.println("`,.,.,");
     }
 }
