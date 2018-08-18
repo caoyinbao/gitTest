764b9781a12f3052f8e0c7d6238a05815350184d
caoyinbao <879992127@qq.com>
2018-08-18 22:07:40 +0800

    增加一个For循环方法

--- src/main/java/GitTest001.java
+++ src/main/java/GitTest001.java
@@ -1,8 +1,15 @@
 public class GitTest001 {
     public static void main(String[] args) {
         sop("12345");
+        ForDemo();
     }
     public static void sop(String str){
         System.out.println(str);
+    }
+    public static void ForDemo(){
+        int[] arr = {1,2,3,4,5};
+        for(int a : arr){
+            System.out.println(a);
+        }
     }
 }
